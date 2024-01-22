69e40ef8e874713acba4b7be3d58f87cfddbf9ff
Caitlin Flynn <flynn.ca@northeastern.edu>
2024-01-22 16:46:57 -0500

    Intial card/button

---
+++ client/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, View, GestureResponderEvent } from 'react-native';
+import { Card } from 'react-native-paper';
+
+interface ClickableCardProps {
+    onPress: () => void;
+    children: JSX.Element[] | JSX.Element;
+  }
+
+  
+  const ClickableCard: React.FC<ClickableCardProps> = ({ onPress, children }) => {
+    return (
+      <Card onPress={onPress}>
+        <Card.Content>
+          {children}
+        </Card.Content>
+      </Card>
+    );
+  };
+
+
+  
