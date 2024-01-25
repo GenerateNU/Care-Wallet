75561d86c732f22cd7e5ef785bba32aaafa8ba26
Olivia Sedarski <osedarski@gmail.com>
2024-01-25 11:46:40 -0500

    Update file model - revisit

--- backend/models/file.go
+++ backend/models/file.go
@@ -1,8 +1,11 @@
 package models
 
 type File struct {
-	FileID   int    `json:"file_id"`
-	FileName string `json:"file_name"`
-	FileSize int64  `json:"file_size"`
-	User     uint   `json:"user"`
+	FileID     int    `json:"file_id"`
+	GroupID    int    `json:"group_id"`
+	UploadBy   int    `json:"upload_by"`
+	UploadDate string `json:"upload_date"`
+	TaskID     int    `json:"task_id"`
+	FileName   string `json:"file_name"`
+	FileSize   int64  `json:"file_size"`
 }
