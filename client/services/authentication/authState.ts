e457aab11271afc1cda9b15c872097f9d7ca0c1c
Matt McCoy <59743922+MattCMcCoy@users.noreply.github.com>
2024-01-29 11:44:23 -0500

    revert auth changes in main (#16)

--- client/services/authentication/authState.ts
+++
@@ -1,8 +0,0 @@
-import { auth } from '../../firebase.config';
-import { onAuthStateChanged as firebaseOnAuthStateChanged, User } from "firebase/auth";
-
-export const onAuthStateChanged = (callback: (user: User | null) => void): void => {
-    firebaseOnAuthStateChanged(auth, (user: User | null) => {
-      callback(user);
-    });
-};
