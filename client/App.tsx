a6652312c656c82f210c00aab22826a2d66521f1
haleymartin-6 <119809061+haleymartin-6@users.noreply.github.com>
2024-01-29 20:17:07 -0500

    new medication page and added frontend nav to the app with landing header and icon

--- client/App.tsx
+++ client/App.tsx
@@ -1,21 +1,53 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { getAllMedications } from './services/medication';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, NavigationProp } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Medication from './screens/Medication';
+import { Image } from 'react-native';
+
+
+export type ScreenNames = [
+  'Landing'
+];
+export type RootStackParamList = Record<ScreenNames[number], any>;
+export type StackNavigation = NavigationProp<RootStackParamList>;
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
+const favicon = require('./assets/favicon.png'); 
 
 export default function App() {
-  const [medications, setMedications] = React.useState<Medication[]>();
-  React.useEffect(() => {
-    getAllMedications().then((med) => setMedications(med));
-  }, []);
-
   return (
-    <View className="flex-1 items-center w-[100vw] justify-center bg-white">
-      {medications &&
-        medications.map((med, index) => (
-          <Text key={index} className="pb-2">
-            {`Name: ${med.medication_name} id: ${med.medication_id}`}
-          </Text>
-        ))}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Landing" options={{ headerShown: true }} component={Tabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { backgroundColor:  'black'}
+      }}
+    >
+      <Tab.Screen
+        name="Devices"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => <Image source={require('./assets/favicon.png')} />,
+          tabBarLabel: () => <Text> Care </Text>
+        }}
+        component={Medication}
+      />
+    </Tab.Navigator>
+  );
+}
+
+
+
+
