4aee3e50286b8f20932162b286cf1e5ce3a36594
narayansharma-21 <97.sharman@gmail.com>
2024-02-05 22:53:44 -0500

    feat(Groups-directory): Added route to add a user to a group role
    Tested on postman and works (as of now)

--- backend/schema/groups/routes.go
+++ backend/schema/groups/routes.go
@@ -2,6 +2,7 @@ package groups
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx"
@@ -11,12 +12,24 @@ type PgModel struct {
 	Conn *pgx.Conn
 }
 
+type CareGroup struct {
+	GroupID     int       `json:"group_id"`
+	GroupName   string    `json:"group_name"`
+	DateCreated time.Time `json:"date_created"`
+}
+
 func CreateCareGroup(v1 *gin.RouterGroup, c *PgModel) *gin.RouterGroup {
 	careGroups := v1.Group("care-groups")
 	{
 		careGroups.POST("/:groupName", func(ctx *gin.Context) {
 			groupName := ctx.Param("groupName")
 			c.CreateCareGroups(ctx, groupName)
+		})
+		careGroups.POST("/addUser/:userId/:groupId/:role", func(ctx *gin.Context) {
+			userId := ctx.Param("userId")
+			groupId := ctx.Param("groupId")
+			role := ctx.Param("role")
+			c.AddUserCareGroup(ctx, userId, groupId, role)
 		})
 	}
 
@@ -34,8 +47,27 @@ func (pg *PgModel) CreateCareGroups(c *gin.Context, groupName string) {
 	careGroups, err := CreateCareGroupsFromDB(pg.Conn, groupName)
 
 	if err != nil {
-		panic(err)
+		c.JSON(http.StatusBadRequest, err.Error())
 	}
 
 	c.JSON(http.StatusOK, careGroups)
+}
+
+// CreateCareGroup godoc
+//
+//	@summary		Adds a user to a care group
+//	@description	Adds a user to a care group given a userID, groupID, and role
+//	@tags			groups
+//	@success		200	{array}		models.CareGroup
+//	@failure		400	{object}	string
+//	@router			/care-groups/addUser/:userId/:groupId/:role [post]
+func (pg *PgModel) AddUserCareGroup(c *gin.Context, userId string, groupId string, role string) {
+	user, err := AddUserCareGroupFromDB(pg.Conn, userId, groupId, role)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	c.JSON(http.StatusOK, user)
+
 }
--- backend/schema/groups/transactions.go
+++ backend/schema/groups/transactions.go
@@ -32,3 +32,29 @@ func CreateCareGroupsFromDB(conn *pgx.Conn, groupName string) (int, error) {
 	return careGroupID, nil
 
 }
+
+func AddUserCareGroupFromDB(conn *pgx.Conn, userId string, groupId string, role string) (int, error) {
+
+	rows, err := conn.Exec("INSERT INTO group_roles (group_id, user_id, role) VALUES ($1, $2, $3)", groupId, userId, role)
+
+	if err != nil {
+		print(err, "from transactions err ")
+		return 0, err
+	}
+
+	// Check the number of rows affected by the insert operation
+	rowsAffected := rows.RowsAffected()
+
+	if rowsAffected != 1 {
+		return 0, errors.New("unexpected number of rows affected")
+	}
+
+	var lastUserId int
+	err = conn.QueryRow("SELECT lastval()").Scan(&lastUserId)
+	if err != nil {
+		return 0, err
+	}
+
+	return lastUserId, nil
+
+}
