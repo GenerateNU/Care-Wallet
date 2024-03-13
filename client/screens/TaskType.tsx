02e36d23a9f76627a84c91a94f908b97b25f3fdd
oliviaseds <osedarski@gmail.com>
2024-03-12 23:02:59 -0400

    fix: remove redundant SafeView, Other button not cut off

--- client/screens/TaskType.tsx
+++ client/screens/TaskType.tsx
@@ -14,7 +14,7 @@ import BottomSheet, {
 import { BottomSheetDefaultBackdropProps } from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types';
 import { useNavigation } from '@react-navigation/native';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Button, Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -71,80 +71,78 @@ export function TaskType() {
   );
 
   return (
-    <GestureHandlerRootView>
-      <SafeAreaView>
-        <View className="flex w-full flex-row items-center justify-center">
-          <View className="mr-[95px]">
-            <BackButton />
-          </View>
-          <Text className="mr-auto self-center text-center text-carewallet-gray">
-            Step 1 of 2
-          </Text>
+    <GestureHandlerRootView className="mt-10">
+      <View className="flex w-full flex-row items-center justify-center">
+        <View className="mr-[95px]">
+          <BackButton />
         </View>
+        <Text className="mr-auto self-center text-center text-carewallet-gray">
+          Step 1 of 2
+        </Text>
+      </View>
 
-        <Text className="text-center text-2xl font-bold">Type of Task</Text>
-        <View className="mr-2 flex flex-row justify-end">
-          <Button
-            className="h-[40px] items-center justify-center rounded-xl text-sm"
-            textColor="black"
-            mode="outlined"
-            onPress={() => snapToIndex(0)}
-          >
-            Filter
-          </Button>
-        </View>
-
-        <FlatList
-          className="h-full"
-          data={selectedTypes}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              className="m-2 h-[50px] overflow-hidden rounded-xl"
-              onPress={() => navigation.navigate('New ' + item + ' Task')}
-            >
-              <Button
-                className="m-2 h-[50px] items-center justify-center rounded-xl"
-                textColor="black"
-                mode="outlined"
-              >
-                {item}
-              </Button>
-            </TouchableOpacity>
-          )}
-        />
-
-        <BottomSheet
-          ref={bottomSheetRef}
-          index={-1}
-          snapPoints={snapPoints}
-          enablePanDownToClose={true}
-          backdropComponent={renderBackdrop}
+      <Text className="text-center text-2xl font-bold">Type of Task</Text>
+      <View className="mr-2 flex flex-row justify-end">
+        <Button
+          className="h-[40px] items-center justify-center rounded-xl text-sm"
+          textColor="black"
+          mode="outlined"
+          onPress={() => snapToIndex(0)}
         >
-          <View>
-            <View className="flex flex-row justify-between">
-              <Text className="m-5 text-2xl font-bold">Filter</Text>
-              <CloseButton onPress={closeBottomSheet} />
-            </View>
+          Filter
+        </Button>
+      </View>
 
-            <DropDownPicker
-              open={open}
-              value={selectedCategory}
-              items={filters}
-              setOpen={setOpen}
-              setValue={setSelectedCategory}
-              placeholder="Category"
-              style={{
-                width: '95%',
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                borderRadius: 0,
-                borderColor: 'transparent',
-                borderBottomColor: 'black'
-              }}
-            />
+      <FlatList
+        className="h-full"
+        data={selectedTypes}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            className="m-2 h-[50px] overflow-hidden rounded-xl"
+            onPress={() => navigation.navigate('New ' + item + ' Task')}
+          >
+            <Button
+              className="m-2 h-[50px] items-center justify-center rounded-xl"
+              textColor="black"
+              mode="outlined"
+            >
+              {item}
+            </Button>
+          </TouchableOpacity>
+        )}
+      />
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        enablePanDownToClose={true}
+        backdropComponent={renderBackdrop}
+      >
+        <View>
+          <View className="flex flex-row justify-between">
+            <Text className="m-5 text-2xl font-bold">Filter</Text>
+            <CloseButton onPress={closeBottomSheet} />
           </View>
-        </BottomSheet>
-      </SafeAreaView>
+
+          <DropDownPicker
+            open={open}
+            value={selectedCategory}
+            items={filters}
+            setOpen={setOpen}
+            setValue={setSelectedCategory}
+            placeholder="Category"
+            style={{
+              width: '95%',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              borderRadius: 0,
+              borderColor: 'transparent',
+              borderBottomColor: 'black'
+            }}
+          />
+        </View>
+      </BottomSheet>
     </GestureHandlerRootView>
   );
 }
