9e7362e27ef631dcfd205686a50fd3385ef6a187
oliviaseds <osedarski@gmail.com>
2024-03-12 22:56:29 -0400

    refactor: export close cross as group for close button

--- client/components/TaskType/CloseButton.tsx
+++ client/components/TaskType/CloseButton.tsx
@@ -2,20 +2,14 @@ import React from 'react';
 
 import { IconButton } from 'react-native-paper';
 
-import Close1 from '../../assets/close/close1.svg';
-import Close2 from '../../assets/close/close2.svg';
+import Close from '../../assets/close.svg';
 
 export function CloseButton({ onPress }: { onPress: () => void }) {
   return (
     <IconButton
       className="align-center m-2 flex h-[50px] w-[52px] justify-center rounded-xl bg-carewallet-gray"
       mode="contained"
-      icon={() => (
-        <>
-          <Close1 style={{ position: 'absolute', top: 20, left: 20 }} />
-          <Close2 style={{ position: 'absolute', top: 20, left: 20 }} />
-        </>
-      )}
+      icon={Close}
       onPress={onPress}
     />
   );
