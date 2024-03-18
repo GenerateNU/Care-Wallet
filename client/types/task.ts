8b33eda8aa03948d3e0d0c4c47c2fef010b3c5c3
bedrockdude10 <dannyrollo4@gmail.com>
2024-03-18 19:12:39 -0400

    feat: updated client task type to match db

--- client/types/task.ts
+++ client/types/task.ts
@@ -1,10 +1,16 @@
 export interface Task {
   task_id: number;
+  task_title: string;
   group_id: number;
   created_by: string;
+  created_date: string;
   start_date: string;
   end_date: string;
   notes: string;
+  repeating: boolean;
+  repeating_interval: string;
+  repeating_end_date: Date;
   task_status: string;
   task_type: string;
+  task_info: JSON;
 }
