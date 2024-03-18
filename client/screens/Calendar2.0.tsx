096136eabb565bf1243957a1fbb65ab80e285763
haleymartin-6 <119809061+haleymartin-6@users.noreply.github.com>
2024-03-18 10:53:01 -0400

    updated calendar2.0

--- client/screens/Calendar2.0.tsx
+++ client/screens/Calendar2.0.tsx
@@ -1,9 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Alert } from 'react-native';
-
-import filter from 'lodash/filter';
-import find from 'lodash/find';
-import groupBy from 'lodash/groupBy';
+import _ from 'lodash';
 import {
   CalendarProvider,
   CalendarUtils,
@@ -11,47 +8,39 @@ import {
   ExpandableCalendar,
   TimelineEventProps,
   TimelineList,
-  TimelineProps
+  TimelineProps,
 } from 'react-native-calendars';
-import { UpdateSources } from 'react-native-calendars/src/expandableCalendar/commons';
-
 import { getDate, timelineEvents } from './timelineEvents';
+
 
 const INITIAL_TIME = { hour: 9, minutes: 0 };
 const EVENTS: TimelineEventProps[] = timelineEvents;
-export default class TimelineCalendarScreen extends Component {
-  state = {
-    currentDate: getDate(),
-    events: EVENTS,
-    eventsByDate: groupBy(EVENTS, (e) =>
-      CalendarUtils.getCalendarDateString(e.start)
-    ) as {
-      [key: string]: TimelineEventProps[];
-    }
-  };
 
-  marked = {
+const TimelineCalendarScreen: React.FC = () => {
+  const [currentDate, setCurrentDate] = useState(getDate());
+  const [eventsByDate, setEventsByDate] = useState(_.groupBy(EVENTS, (e) => CalendarUtils.getCalendarDateString(e.start)));
+
+  const marked = {
     [`${getDate(-1)}`]: { marked: true },
     [`${getDate()}`]: { marked: true },
     [`${getDate(1)}`]: { marked: true },
     [`${getDate(2)}`]: { marked: true },
-    [`${getDate(4)}`]: { marked: true }
+    [`${getDate(4)}`]: { marked: true },
   };
 
-  onDateChanged = (date: string, source: string) => {
+  const onDateChanged = (date: string, source: string) => {
     console.log('TimelineCalendarScreen onDateChanged: ', date, source);
-    this.setState({ currentDate: date });
+    setCurrentDate(date);
   };
 
-  onMonthChange = (month: DateData, updateSource: UpdateSources) => {
-    console.log('TimelineCalendarScreen onMonthChange: ', month, updateSource);
+  const onMonthChange = (month: DateData, updateSource: any) => {
+  console.log('TimelineCalendarScreen onMonthChange: ', month, updateSource);
   };
 
-  createNewEvent: TimelineProps['onBackgroundLongPress'] = (
+  const createNewEvent: TimelineProps['onBackgroundLongPress'] = (
     timeString,
     timeObject
   ) => {
-    const { eventsByDate } = this.state;
     const hourString = `${(timeObject.hour + 1).toString().padStart(2, '0')}`;
     const minutesString = `${timeObject.minutes.toString().padStart(2, '0')}`;
 
@@ -60,111 +49,100 @@ export default class TimelineCalendarScreen extends Component {
       start: `${timeString}`,
       end: `${timeObject.date} ${hourString}:${minutesString}:00`,
       title: 'New Event',
-      color: 'white'
+      color: 'white',
     };
 
     if (timeObject.date) {
       if (eventsByDate[timeObject.date]) {
-        eventsByDate[timeObject.date] = [
-          ...eventsByDate[timeObject.date],
-          newEvent
-        ];
-        this.setState({ eventsByDate });
+        setEventsByDate((prevEvents) => ({
+          ...prevEvents,
+          [timeObject.date as string]: [...prevEvents[timeObject.date as string], newEvent],
+        }));
       } else {
-        eventsByDate[timeObject.date] = [newEvent];
-        this.setState({ eventsByDate: { ...eventsByDate } });
+        setEventsByDate((prevEvents) => ({
+          ...prevEvents,
+          [timeObject.date as string]: [newEvent],
+        }));
       }
     }
   };
 
-  approveNewEvent: TimelineProps['onBackgroundLongPressOut'] = (
+  const approveNewEvent: TimelineProps['onBackgroundLongPressOut'] = (
     _timeString,
     timeObject
   ) => {
-    const { eventsByDate } = this.state;
-
     Alert.prompt('New Event', 'Enter event title', [
       {
         text: 'Cancel',
         onPress: () => {
           if (timeObject.date) {
-            eventsByDate[timeObject.date] = filter(
-              eventsByDate[timeObject.date],
-              (e) => e.id !== 'draft'
-            );
-
-            this.setState({
-              eventsByDate
-            });
+            setEventsByDate((prevEvents) => ({
+              ...prevEvents,
+              [timeObject.date as string]: _.filter(
+                prevEvents[timeObject.date as string],
+                (e) => e.id !== 'draft'
+              ),
+            }));
           }
-        }
+        },
       },
       {
         text: 'Create',
         onPress: (eventTitle) => {
           if (timeObject.date) {
-            const draftEvent = find(eventsByDate[timeObject.date], {
-              id: 'draft'
+            const draftEvent = _.find(eventsByDate[timeObject.date], {
+              id: 'draft',
             });
             if (draftEvent) {
               draftEvent.id = undefined;
               draftEvent.title = eventTitle ?? 'New Event';
               draftEvent.color = 'lightgreen';
-              eventsByDate[timeObject.date] = [
-                ...eventsByDate[timeObject.date]
-              ];
-
-              this.setState({
-                eventsByDate
-              });
+              setEventsByDate((prevEvents) => ({
+                ...prevEvents,
+                [timeObject.date as string]: [...prevEvents[timeObject.date as string]],
+              }));
             }
           }
-        }
-      }
+        },
+      },
     ]);
   };
 
-  private timelineProps: Partial<TimelineProps> = {
+  const timelineProps: Partial<TimelineProps> = {
     format24h: false,
-    onBackgroundLongPress: this.createNewEvent,
-    onBackgroundLongPressOut: this.approveNewEvent,
-    // scrollToFirst: true,
-    // start: 0,
-    // end: 24,
+    onBackgroundLongPress: createNewEvent,
+    onBackgroundLongPressOut: approveNewEvent,
     unavailableHours: [
       { start: 0, end: 6 },
-      { start: 22, end: 24 }
+      { start: 22, end: 24 },
     ],
     onEventPress: (e) => expandEvent(e),
     overlapEventsSpacing: 8,
-    rightEdgeSpacing: 24
+    rightEdgeSpacing: 24,
   };
 
-  render() {
-    const { currentDate, eventsByDate } = this.state;
+  return (
+    <CalendarProvider
+      date={currentDate}
+      onDateChanged={onDateChanged}
+      onMonthChange={onMonthChange}
+      showTodayButton
+      disabledOpacity={0.6}
+    >
+      <ExpandableCalendar firstDay={1} markedDates={marked} />
+      <TimelineList
+        events={eventsByDate}
+        timelineProps={timelineProps}
+        showNowIndicator
+        scrollToFirst
+        initialTime={INITIAL_TIME}
+      />
+    </CalendarProvider>
+  );
+};
 
-    return (
-      <CalendarProvider
-        date={currentDate}
-        onDateChanged={this.onDateChanged}
-        onMonthChange={this.onMonthChange}
-        showTodayButton
-        disabledOpacity={0.6}
-        // numberOfDays={3}
-      >
-        <ExpandableCalendar firstDay={1} markedDates={this.marked} />
-        <TimelineList
-          events={eventsByDate}
-          timelineProps={this.timelineProps}
-          showNowIndicator
-          // scrollToNow
-          scrollToFirst
-          initialTime={INITIAL_TIME}
-        />
-      </CalendarProvider>
-    );
-  }
-}
 function expandEvent(e: TimelineEventProps): void {
   console.log('expand event', e.title);
 }
+
+export default TimelineCalendarScreen;
