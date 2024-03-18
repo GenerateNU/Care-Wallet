eba4ce360e3264d6ad6a59ddfba4f22b8e61c9b6
oliviaseds <osedarski@gmail.com>
2024-03-18 19:10:39 -0400

    fix: so Matt can see

--- client/screens/TaskCreation.tsx
+++ client/screens/TaskCreation.tsx
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native';
 import { RouteProp, useRoute } from '@react-navigation/native';
 
 import { BackButton } from '../components/TaskType/BackButton';
+import styleJsonData from '../types/task-creation.json';
 
 type ParamList = {
   mt: {
@@ -11,9 +12,27 @@ type ParamList = {
   };
 };
 
+type StyleJsonType = {
+  [key: string]: {
+    Header: string;
+    Body: {
+      [key: string]:
+        | string
+        | {
+            [key: string]: string;
+          };
+    };
+  };
+};
+
 export function TaskCreation() {
   const route = useRoute<RouteProp<ParamList, 'mt'>>();
   const { type } = route.params;
+
+  const styleJson: StyleJsonType = styleJsonData;
+  console.log(styleJson);
+  console.log(type);
+  console.log(styleJson[type]);
 
   return (
     <View className="mt-10">
