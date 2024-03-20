967c9e846a5fa1ef451de2a91d2b6b84096edcdb
oliviaseds <osedarski@gmail.com>
2024-03-19 20:18:42 -0400

    style: formatting

--- client/components/task_creation/AddressComponent.tsx
+++ client/components/task_creation/AddressComponent.tsx
@@ -9,18 +9,19 @@ export function AddressComponent() {
         <TextInput
           className="border-gray-300 w-full rounded-md border px-4 py-2"
           placeholder={'Fill in blank'}
+          autoComplete="street-address"
         />
       </View>
 
       <View className="m-4 mb-0 flex flex-row">
-        <View className="w-1/2">
+        <View className="w-[49%]">
           <Text className="mb-2">City</Text>
           <TextInput
             className="border-gray-300 w-full rounded-md border px-4 py-2"
             placeholder={'Fill in blank'}
           />
         </View>
-        <View className="ml-2 w-1/2">
+        <View className="ml-2 w-[49%]">
           <Text className="mb-2">State</Text>
           <TextInput
             className="border-gray-300 w-full rounded-md border px-4 py-2"
@@ -30,19 +31,21 @@ export function AddressComponent() {
       </View>
 
       <View className="m-4 mb-0 flex flex-row">
-        <View className="w-1/2">
+        <View className="w-[49%]">
           <Text className="mb-2">Zip Code</Text>
           <TextInput
             className="border-gray-300 w-full rounded-md border px-4 py-2"
             placeholder={'Fill in blank'}
+            autoComplete="postal-code"
             keyboardType="numeric"
           />
         </View>
-        <View className="ml-2 w-1/2">
+        <View className="ml-2 w-[49%]">
           <Text className="mb-2">Phone Number</Text>
           <TextInput
             className="border-gray-300 w-full rounded-md border px-4 py-2"
             placeholder={'Fill in blank'}
+            autoComplete="tel"
             keyboardType="phone-pad"
           />
         </View>
--- client/screens/TaskCreation.tsx
+++ client/screens/TaskCreation.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 import { RouteProp, useRoute } from '@react-navigation/native';
+import {
+  GestureHandlerRootView,
+  ScrollView
+} from 'react-native-gesture-handler';
 
 import { BackButton } from '../components/nav_buttons/BackButton';
+import { AddressComponent } from '../components/task_creation/AddressComponent.tsx';
 import { RadioGroup } from '../components/task_creation/RadioGroup.tsx';
 import { TextInputLine } from '../components/task_creation/TextInputLine.tsx';
 import { TextInputParagraph } from '../components/task_creation/TextInputParagraph.tsx';
@@ -27,22 +32,26 @@ export function TaskCreation() {
   console.log(body);
 
   return (
-    <View className="mt-10">
-      <View className="flex w-full flex-row items-center justify-center">
-        <View className="mr-[95px]">
-          <BackButton />
+    <GestureHandlerRootView className="my-10">
+      <ScrollView>
+        <View className="flex w-full flex-row items-center justify-center">
+          <View className="mr-[95px]">
+            <BackButton />
+          </View>
+          <Text className="mr-auto self-center text-center text-carewallet-gray">
+            Step 1 of 2
+          </Text>
         </View>
-        <Text className="mr-auto self-center text-center text-carewallet-gray">
-          Step 1 of 2
-        </Text>
-      </View>
+        <Text className="text-center text-2xl font-bold">{header}</Text>
 
-      <Text className="text-center text-2xl font-bold">{header}</Text>
-      <TextInputLine title={'Drug Name'} />
-      <RadioGroup title={'Drug Form'} options={['Pill', 'Liquid', 'Shot']} />
-      <TextInputLine title={'Diagnosis'} />
-      <TextInputLine title={'Prescribing Physician'} />
-      <TextInputParagraph title={'Care Instructions'} />
-    </View>
+        <TextInputLine title={'Drug Name'} />
+        <RadioGroup title={'Drug Form'} options={['Pill', 'Liquid', 'Shot']} />
+        <TextInputLine title={'Diagnosis'} />
+        <TextInputLine title={'Prescribing Physician'} />
+        <TextInputParagraph title={'Care Instructions'} />
+        <AddressComponent />
+        <AddressComponent />
+      </ScrollView>
+    </GestureHandlerRootView>
   );
 }
