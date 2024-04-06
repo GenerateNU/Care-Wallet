3b07d84ac245e90cfe9863fa8b81a1fc6148b46d
oliviaseds <osedarski@gmail.com>
2024-04-06 12:52:16 -0400

    feat: radio button icons

--- client/components/task_creation/RadioGroup.tsx
+++ client/components/task_creation/RadioGroup.tsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 import ButtonCircle from '../../assets/radio-button-circle.svg';
+import Bathing from '../../assets/task-creation/bathing.svg';
+import Liquid from '../../assets/task-creation/liquid.svg';
+import Pill from '../../assets/task-creation/pill.svg';
+import Shot from '../../assets/task-creation/shot.svg';
+import Toileting from '../../assets/task-creation/toileting.svg';
 
 interface RadioGroupProps {
   title: string;
@@ -20,26 +25,45 @@ export function RadioGroup({ title, options, onChange }: RadioGroupProps) {
     }
   };
 
+  const renderIcon = (option: string) => {
+    switch (option) {
+      case 'Pills':
+        return <Pill />;
+      case 'Liquid':
+        return <Liquid />;
+      case 'Shot':
+        return <Shot />;
+      case 'Bathing':
+        return <Bathing />;
+      case 'Toileting':
+        return <Toileting />;
+      default:
+        return <ButtonCircle />;
+    }
+  };
+
   return (
     <View className="m-4 mb-0">
       <Text className="mb-2 font-carewallet-montserrat-semibold text-[14px]">
         {title.toUpperCase()}
       </Text>
       <View className="flex flex-row justify-between">
-        {options.map((option, index) => (
-          <TouchableOpacity
-            key={index}
-            className={`flex h-12 flex-row items-center space-x-2 rounded-md border border-carewallet-gray px-4 py-2 ${option === selectedOption ? 'bg-carewallet-gray' : 'bg-carewallet-white'}`}
-            onPress={() => {
-              handleOptionSelect(option);
-            }}
-          >
-            <ButtonCircle />
-            <Text className="font-carewallet-manrope-semibold text-[16px]">
-              {option}
-            </Text>
-          </TouchableOpacity>
-        ))}
+        {options.map((option, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              className={`flex h-12 flex-row items-center space-x-2 rounded-md border border-carewallet-gray px-4 py-2 ${option === selectedOption ? 'bg-carewallet-gray' : 'bg-carewallet-white'}`}
+              onPress={() => {
+                handleOptionSelect(option);
+              }}
+            >
+              {renderIcon(option)}
+              <Text className="font-carewallet-manrope-semibold text-[16px]">
+                {option}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
       </View>
     </View>
   );
