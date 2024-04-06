1021308dbed406aaf338e4be7eec2109b2908d51
narayansharma-21 <97.sharman@gmail.com>
2024-04-06 16:17:16 -0400

    style(taskType): fixing

--- client/screens/TaskType.tsx
+++ client/screens/TaskType.tsx
@@ -13,8 +13,12 @@ import BottomSheet, {
 } from '@gorhom/bottom-sheet';
 import { BottomSheetDefaultBackdropProps } from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types';
 import { useNavigation } from '@react-navigation/native';
+import { List } from 'lodash';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {
+  GestureHandlerRootView,
+  ScrollView
+} from 'react-native-gesture-handler';
 import { Button, Text } from 'react-native-paper';
 
 import { BackButton } from '../components/nav_buttons/BackButton';
@@ -70,82 +74,139 @@ export function TaskType() {
     []
   );
 
+  const TypeToCategoryMap: Record<string, Category> = {
+    'Medication Management': Category.HEALTH,
+    Diet: Category.HOME,
+    Grooming: Category.PERSONAL
+  };
+
+  function getCards(item: TypeOfTask): string[] {
+    const card = TypeToCategoryMap[item];
+
+    switch (card) {
+      case Category.HEALTH:
+        return ['Medication Management', 'Physician Appointments'];
+      case Category.FINANCIAL:
+        return ['Health Insurance'];
+      case Category.HOME:
+        return ['Diet', 'Activities'];
+      case Category.PERSONAL:
+        return [
+          'Grooming',
+          'Shopping & Errands',
+          'Family Conversations',
+          'Pay Bills'
+        ];
+      default:
+        return [];
+    }
+  }
+
+  const list = {
+    'Health & Medical': ['Medication Management', 'Physician Appointments'],
+    HomeLifestyle: ['Diet', 'Activities'],
+    Personal: ['Grooming', 'Shopping & Errands', 'Family Conversations']
+  };
+
+  function getCategoryTitle(item: TypeOfTask): string {
+    const category = TypeToCategoryMap[item];
+    switch (category) {
+      case Category.HEALTH:
+        return 'Health & Medical';
+      case Category.PERSONAL:
+        return 'Personal';
+      case Category.HOME:
+        return 'Home & Lifestyle';
+      case Category.FINANCIAL:
+        return 'Financial & Legal';
+      case Category.OTHER:
+        return 'Other';
+      default:
+        return ''; // Default case if item doesn't match any category
+    }
+  }
+
   return (
     <GestureHandlerRootView className="bg-carewallet-white pt-10">
-      <View className="flex w-full flex-row items-center">
-        <BackButton />
+      <ScrollView>
+        <View className="flex w-full flex-row items-center">
+          <BackButton />
 
-        <Text className="mx-auto pr-20 font-carewallet-manrope-bold text-[18px] text-carewallet-blue">
-          Step 1 of 3
-        </Text>
-      </View>
-      <View className="my-5 border-b border-carewallet-lightgray" />
-
-      <View className="flex w-full flex-row items-start justify-between px-4">
-        <Text className="font-carewallet-manrope-bold text-[24px]">
-          Choose Type of Task
-        </Text>
-        <Button
-          className=" h-14 items-center justify-center rounded-lg bg-carewallet-blue text-carewallet-white"
-          textColor="white"
-          onPress={() => snapToIndex(0)}
-        >
-          FILTER
-        </Button>
-      </View>
-
-      <View className="my-2" />
-      <View className="flex h-[68vh] flex-wrap justify-between p-4">
-        {selectedTypes.map((item, index) => (
-          <TouchableOpacity
-            key={index}
-            onPress={() =>
-              navigation.navigate('TaskCreation', {
-                taskType: JSON.stringify(item)
-              })
-            }
-          >
-            <View className="border-gray-400 bg-white mx-2 flex h-24 w-40 flex-col items-start rounded-lg border p-2">
-              <Text className="flex-shrink flex-grow">{item}</Text>
-            </View>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={-1}
-        snapPoints={bottomSheetSnapPoints}
-        enablePanDownToClose={true}
-        backdropComponent={renderBackdrop}
-      >
-        <View>
-          <View className="flex flex-row justify-between">
-            <Text className="m-5 text-2xl font-bold">Filter</Text>
-            <CloseButton onPress={closeBottomSheet} />
-          </View>
-
-          <DropDownPicker
-            open={open}
-            value={selectedCategory}
-            items={filters}
-            setOpen={setOpen}
-            setValue={setSelectedCategory}
-            placeholder="Category"
-            onSelectItem={() => {
-              closeBottomSheet();
-            }}
-            style={{
-              width: '95%',
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              borderRadius: 0,
-              borderColor: 'transparent',
-              borderBottomColor: 'black'
-            }}
-          />
+          <Text className="mx-auto pr-20 font-carewallet-manrope-bold text-[18px] text-carewallet-blue">
+            Step 1 of 3
+          </Text>
         </View>
-      </BottomSheet>
+        <View className="my-5 border-b border-carewallet-lightgray" />
+
+        <View className="flex w-full flex-row items-start justify-between px-4">
+          <Text className="font-carewallet-manrope-bold text-[24px]">
+            Choose Type of Task
+          </Text>
+          <Button
+            className=" h-14 items-center justify-center rounded-lg bg-carewallet-blue text-carewallet-white"
+            textColor="white"
+            onPress={() => snapToIndex(0)}
+          >
+            FILTER
+          </Button>
+        </View>
+
+        <View className="my-2" />
+
+        <View className="flex h-[68vh] w-[90vw] flex-col flex-wrap justify-between p-4">
+          {selectedTypes.map((item, index) => (
+            <View key={index}>
+              <Text className="mx-1 p-2">{getCategoryTitle(item)}</Text>
+              <View className="w-[90vw] flex-col flex-wrap">
+                {getCards(item).map((item2, index2) => (
+                  <View
+                    key={index2}
+                    className="border-gray-400 bg-white m-2 h-24 w-40 items-start rounded-lg border p-2"
+                    onTouchCancel={() => {}}
+                  >
+                    <Text>{item2}</Text>
+                  </View>
+                ))}
+              </View>
+            </View>
+          ))}
+        </View>
+
+        <BottomSheet
+          ref={bottomSheetRef}
+          index={-1}
+          snapPoints={bottomSheetSnapPoints}
+          enablePanDownToClose={true}
+          backdropComponent={renderBackdrop}
+        >
+          <View>
+            <View className="flex flex-row justify-between">
+              <Text className="m-5 text-2xl font-bold">Filter</Text>
+              <CloseButton onPress={closeBottomSheet} />
+            </View>
+
+            <DropDownPicker
+              open={open}
+              value={selectedCategory}
+              items={filters}
+              setOpen={setOpen}
+              setValue={setSelectedCategory}
+              placeholder="Category"
+              onSelectItem={() => {
+                closeBottomSheet();
+              }}
+              style={{
+                width: '95%',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                borderRadius: 0,
+                borderColor: 'transparent',
+                borderBottomColor: 'black'
+              }}
+            />
+          </View>
+        </BottomSheet>
+      </ScrollView>
     </GestureHandlerRootView>
   );
 }
