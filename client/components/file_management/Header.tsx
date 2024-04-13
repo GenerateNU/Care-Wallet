67f0cc382fb378ab3061cfa578f74a5f1b90525d
patela22 <39590072+patela22@users.noreply.github.com>
2024-04-13 13:17:15 -0400

    fix(text): font

--- client/components/file_management/Header.tsx
+++ client/components/file_management/Header.tsx
@@ -10,16 +10,16 @@ export function Header() {
   const navigation = useNavigation<AppStackNavigation>();
   return (
     <SafeAreaView className="bg-carewallet-white/80">
-      <View className="flex flex-row items-center justify-between border-b border-carewallet-lightgray bg-carewallet-white pb-4">
+      <View className="carewallet-manrope-bold flex flex-row items-center justify-between border-b border-carewallet-lightgray bg-carewallet-white pb-4">
         <View className="pl-1">
           <BackButton />
         </View>
-        <Text className="mx-auto font-carewallet-montserrat-semibold text-xl text-carewallet-blue">
+        <Text className="mx-auto font-carewallet-manrope-bold text-xl text-carewallet-blue">
           View Files
         </Text>
         <View className="">
           <View
-            className="right-3 h-12 w-12 items-center justify-center rounded-xl bg-carewallet-blue"
+            className="right-3 h-12 w-12 items-center justify-center rounded-xl bg-carewallet-blue font-carewallet-manrope"
             onTouchEnd={() => navigation.navigate('FileUploadScreen')}
           >
             <Text className="text-4xl text-carewallet-white">+</Text>
